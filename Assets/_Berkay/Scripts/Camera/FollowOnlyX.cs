9cbff69aea2886c4c5a15025c5ca6ee7120a2cd5
Berkay Dindar <berkay97d@gmail.com>
2024-03-16 18:59:21 +0300

    Changes

--- Assets/_Berkay/Scripts/Camera/FollowOnlyX.cs
+++ Assets/_Berkay/Scripts/Camera/FollowOnlyX.cs
@@ -1,13 +1,15 @@
 ﻿using UnityEngine;
 using Cinemachine;
- 
+using Unity.VisualScripting;
+
 
 [ExecuteInEditMode] [SaveDuringPlay] [AddComponentMenu("")] // Hide in menu
 public class FollowOnlyX : CinemachineExtension
 {
     [Tooltip("Lock the camera's Z position to this value")]
     public float m_YPosition = -10;
- 
+
+    
     protected override void PostPipelineStageCallback(
         CinemachineVirtualCameraBase vcam,
         CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
@@ -15,7 +17,9 @@ public class FollowOnlyX : CinemachineExtension
         if (stage == CinemachineCore.Stage.Body)
         {
             var pos = state.RawPosition;
+            
             pos.y = m_YPosition;
+            
             state.RawPosition = pos;
         }
     }
